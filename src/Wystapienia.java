f530d70b41f3c9e781222403547c9bb7675e20fa
Agnieszka Łozowska <aga.lozowska@wp.pl>
2018-08-12 20:59:47 +0200

    Pierwsza wersja programu

---
+++ src/Wystapienia.java
@@ -0,0 +1,26 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+import java.util.TreeMap;
+
+public class Wystapienia {
+    public static void main(String[] args) throws FileNotFoundException {
+        File file = new File("liczby.txt");
+        TreeMap<Integer, Integer> set=new TreeMap<>();
+        Scanner scanner = new Scanner(file);
+        Integer [] tablica=new Integer[6];
+        Integer licznik=0;
+        for (int i = 0; i < tablica.length; i++) {
+          tablica[i]=scanner.nextInt();
+          Integer singelNumber=tablica[i];
+          licznik=(Integer)set.get(singelNumber);
+          if (licznik==null){
+              licznik=Integer.valueOf(0);
+          }
+          licznik++;
+          set.put(singelNumber,licznik);
+        }
+        System.out.println(set);
+
+    }
+}
